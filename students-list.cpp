7317e92eedf467dfc3048f69e3fd137b34b940f7
seara2k <seara2k@users.noreply.github.com>
2020-03-31 20:32:46 +0300

    Update students-list.cpp

--- students-list.cpp
+++ students-list.cpp
@@ -18,8 +18,11 @@ struct student {
 		month_of_birth =  new_month_of_birth;
 		year_of_birth = new_year_of_birth;
 	}
-	void name(const&int K) {
-		cout << student.name << " " << student.surname << endl
+	void name_print() {
+		cout << name << " " << surname << endl;
+	}
+	void date_print() {
+		cout << day_of_birth << " " << month_of_birth << " " << year_of_birth << " " << endl;
 	}
 	string name;
 	string surname;
@@ -29,25 +32,22 @@ struct student {
 };
 
 int main() {
-	int count, number;
+	int count, k;
 	string input1, input2;
 	int input3, input4, input5;
 	vector<student> list;
 	cin >> count;
-	for (int i; i < count; ++i) {
+	for (int i = 0; i < count; ++i) {
 		cin >> input1 >> input2 >> input3 >> input4 >> input5;
 		list.push_back({input1 , input2 , input3 , input4, input5});
 	}
 	cin >> count;
-	for (int i; i < count; ++i) {
-		cin >> input1;
-		cin >> number;
-		if (input1 == "date") {
+	for (int i = 0; i < count; ++i) {
+		cin >> input1 >> k;
 
-
-
-
-		}
+		if (input1 == "name") list[k - 1].name_print();
+		else if (input1 == "date") list[k - 1].date_print();
+		else cout << "lol";
 	}
 	return 0;
 
