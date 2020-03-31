2c6433b12a50727644892fd9b01bfae345b8f614
seara2k <seara2k@users.noreply.github.com>
2020-03-31 20:00:07 +0300

    Update students-list.cpp

--- students-list.cpp
+++ students-list.cpp
@@ -2,46 +2,17 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
 
 
-
-
-
 struct student {
-//public:
-	student(const string& new_name,
-	        const string& new_surname,
-	        const int& new_day_of_birth,
-	        const int& new_month_of_birth,
-	        const int& new_year_of_birth)
-	{
-
-		name = new_name;
-		surname =  new_surname;
-		day_of_birth = 	new_day_of_birth;
-		month_of_birth =  new_month_of_birth;
-		year_of_birth = new_year_of_birth;
-
-
-	}
-//private:
-	string name;
-	string surname;
-	int day_of_birth;
-	int month_of_birth;
-	int year_of_birth;
-};
-
-
-struct student {
-
-	student(const string& new_name,
-	        const string& new_surname,
-	        const int& new_day_of_birth,
-	        const int& new_month_of_birth,
-	        const int& new_year_of_birth)
+	student (const string& new_name,
+	         const string& new_surname,
+	         const int& new_day_of_birth,
+	         const int& new_month_of_birth,
+	         const int& new_year_of_birth)
 	{
 		name = new_name;
 		surname =  new_surname;
@@ -51,31 +22,33 @@ struct student {
 	}
 
 
-	void add(const string& new_name,
+	/*void add(const string& new_name,
 	         const string& new_surname,
 	         const int& new_day_of_birth,
 	         const int& new_month_of_birth,
 	         const int& new_year_of_birth) {
 		sets.push_back({new_name, new_surname, new_day_of_birth, new_month_of_birth, new_year_of_birth});
-	}
-	void name(const int& K) {
+	}*/
+	/*void name(const int& K) {
 		cout << sets[K - 1][0] << " " << sets[K - 1][1];
-	}
+	}*/
 
 	string name;
 	string surname;
 	int day_of_birth;
 	int month_of_birth;
 	int year_of_birth;
-
 };
 
 
 int main() {
 	int count;
-	God God;
-	God.add("Ivan", "Ivanov", 1, 1, 1901);
-	God.add("Petr", "Petrox", 2, 2, 1902);
-	God.name(1);
+	vector<student> list();
+	cin >> count;
+	for (int i; i < count; ++i) {
+		cin >> input1 >> input2 >> input3 >> input4;
+		list.push_back(input1 , input2 , input3 , input4)
+	}
+	cout << (list[0]).day_of_birth;
 	return 0;
 }
