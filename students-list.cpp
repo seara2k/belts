03d2d26898bc51f8d9325a38a76abe6447118ca5
seara2k <seara2k@users.noreply.github.com>
2020-03-31 18:49:15 +0300

    Create students-list.cpp

---
+++ students-list.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+
+
+
+
+class student {
+	void student
+	string name;
+	string surname;
+	int day_of_birth;
+	int month_of_birth;
+	int year_of_birth;
+}
+
+
+class God {
+	void name(K){
+		cout<<sets[]
+	}
+
+
+	private:
+		vector<student> sets;
+}
+
+
+int main() {
+	
+	return 0;
+}
