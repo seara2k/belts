3b3af5e2a98577c32646a4f3c8b1c237d515c9f3
seara2k <seara2k@users.noreply.github.com>
2020-03-31 19:01:46 +0300

    Update students-list.cpp

--- students-list.cpp
+++ students-list.cpp
@@ -10,7 +10,23 @@ using namespace std;
 
 
 class student {
-	void student
+public:
+	student(const string& new_name,
+	        const string& new_surname,
+	        const int& new_day_of_birth,
+	        const int& new_month_of_birth,
+	        const int& new_year_of_birth)
+	{
+
+		name = new_name;
+		surname =  new_surname;
+		day_of_birth = 	new_day_of_birth;
+		month_of_birth =  new_month_of_birth;
+		year_of_birth = new_year_of_birth;
+
+
+	}
+private:
 	string name;
 	string surname;
 	int day_of_birth;
@@ -20,17 +36,17 @@ class student {
 
 
 class God {
-	void name(K){
-		cout<<sets[]
+	void name(K) {
+		cout << sets[]
 	}
 
 
-	private:
-		vector<student> sets;
+private:
+	vector<student> sets;
 }
 
 
 int main() {
-	
+
 	return 0;
 }
